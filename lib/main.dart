68283990bff1981b81a02b2bf157e97aa6c8fa7d
Cristian Andres Blandon <cblandon@playtechla.com>
2022-10-07 16:30:36 -0500

    modificacion codigo orientacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:peliculas/providers/movies_provider.dart';
 import 'package:peliculas/screens/screens.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp( estadoApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+      WidgetsFlutterBinding.ensureInitialized();
+  runApp(const EstadoApp());
+}
 
-class estadoApp extends StatelessWidget {
-
+class EstadoApp extends StatelessWidget {
+  const EstadoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => MoviesProvider(),lazy: false,)
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        )
       ],
       child: MyApp(),
     );
@@ -20,23 +30,18 @@ class estadoApp extends StatelessWidget {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Pequilulas',
-      initialRoute: 'home',
-      routes:{
-        'home':(context) => HomeScreen(),
-        'details':(context) => DetailsScreen(),
-        'Prueba':(context) => Prueba(),
-      } ,
-      theme:ThemeData.light().copyWith(
-        appBarTheme: AppBarTheme(
-          color: Colors.indigo
-        )
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Pequilulas',
+        initialRoute: 'home',
+        routes: {
+          'home': (context) => HomeScreen(),
+          'details': (context) => DetailsScreen(),
+          'Prueba': (context) => Prueba(),
+        },
+        theme: ThemeData.light()
+            .copyWith(appBarTheme: AppBarTheme(color: Colors.indigo)));
   }
 }
